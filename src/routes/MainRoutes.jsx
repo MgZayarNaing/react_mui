45b52d2c5dfc2729b5af4a206df838f2eddbf49f
Zayar Naing <zayarnaing.st@gmail.com>
2024-08-04 09:48:00 +0630

    frame

---
+++ src/routes/MainRoutes.jsx
@@ -0,0 +1,39 @@
+import { lazy } from 'react';
+
+// project import
+import Loadable from 'components/Loadable';
+import Dashboard from 'layout/Dashboard';
+
+const Color = Loadable(lazy(() => import('pages/component-overview/color')));
+const DashboardDefault = Loadable(lazy(() => import('pages/dashboard/index')));
+
+
+
+// ==============================|| MAIN ROUTING ||============================== //
+
+const MainRoutes = {
+  path: '/',
+  element: <Dashboard />,
+  children: [
+    {
+      path: '/',
+      element: <DashboardDefault />
+    },
+    {
+      path: 'color',
+      element: <Color />
+    },
+    {
+      path: 'dashboard',
+      children: [
+        {
+          path: 'default',
+          element: <DashboardDefault />
+        }
+      ]
+    },
+    
+  ]
+};
+
+export default MainRoutes;
